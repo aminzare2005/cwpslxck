c9a125ac88e201816b01024feb790f41fb4c2786
Amin Zare <142733089+aminzare2005@users.noreply.github.com>
2025-12-22 08:48:06 +0000

    About: remove skills section

--- Components/sections/about.tsx
+++ Components/sections/about.tsx
@@ -4,15 +4,6 @@ import Link from "next/link";
 import { Download } from "lucide-react";
 
 function About() {
-  const soft_skills = ["Git", "Swagger", "Figma", "v0", "bolt.new"];
-  const hard_skills = [
-    "NextJS",
-    "Tailwindcss",
-    "React-Query",
-    "Expo",
-    "React Bits",
-    "ShadcnUI",
-  ];
   return (
     <Section classNameWrapper="space-y-3 flex flex-col justify-center items-center">
       <Link
@@ -31,24 +22,6 @@ function About() {
         درکنارش بصورت پاره‌وقت برای برندهای اینستاگرامی و کامیونیتی‌ها
         <br />
         تولیدمحتوا، طراحی گرافیکی و ادیت ویدیو انجام میدم.
-      </div>
-      <div className="w-full flex justify-center flex-wrap gap-2">
-        {soft_skills.map((s, i) => (
-          <div
-            className="px-1.5 py-0.5 border border-zinc-400 hover:bg-black hover:border-black hover:text-white rounded-lg duration-300"
-            key={i}
-          >
-            {s}
-          </div>
-        ))}
-        {hard_skills.map((s, i) => (
-          <div
-            className="px-1.5 py-0.5 border hover:bg-violet-600 hover:text-white duration-300 hover:border-violet-600 border-violet-300 rounded-lg"
-            key={i}
-          >
-            {s}
-          </div>
-        ))}
       </div>
     </Section>
   );
