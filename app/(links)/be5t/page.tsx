2fe20715de6a01e2afebef7f93bdb857e1cb2b60
Amin Zare <2005aminzare@gmail.com>
2026-02-11 14:00:09 +0330

    update links

---
+++ app/(links)/be5t/page.tsx
@@ -0,0 +1,4 @@
+import { redirect } from "next/navigation";
+export default function page() {
+  redirect("https://be5t.ir/validation/cert?id=aa54c49a-86ea-4863-a500-98998cb23912");
+}
--- app/(links)/tiktok/page.tsx
+++
@@ -1,4 +0,0 @@
-import { redirect } from "next/navigation";
-export default function page() {
-  redirect("https://tiktok.com/@cwpslxck");
-}
